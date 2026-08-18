0101ff85a5bfa964ddd523d8dd4a6c11f6bd660a
Vicky <vigneshwara4011.com>
2026-08-18 19:35:46 +0530

    Connect Streamlit frontend to FastAPI

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import requests
+
+
+API_URL = "http://127.0.0.1:8000/scrape"
 
 
 st.title("Web Scraper")
@@ -12,7 +16,29 @@ url = st.text_input(
 
 if st.button("Scrape Website"):
 
-    if url:
-        st.success(f"URL entered: {url}")
-    else:
+    if not url:
         st.warning("Please enter a URL.")
+
+    else:
+        try:
+            response = requests.post(
+                API_URL,
+                json={
+                    "url": url
+                }
+            )
+
+            if response.status_code == 200:
+                data = response.json()
+
+                st.success("Website scraped successfully!")
+
+                st.json(data)
+
+            else:
+                st.error(
+                    f"Scraping failed: {response.text}"
+                )
+
+        except requests.exceptions.RequestException:
+            st.error("Could not connect to the FastAPI server.")
